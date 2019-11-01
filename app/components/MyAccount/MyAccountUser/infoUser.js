6e2202f7766a90bf7c916845f252d35dd0b20cd1
UAHM <uahm93@gmail.com>
2019-10-31 18:56:01 -0600

    Funcion de actualizar avatar

--- app/components/MyAccount/MyAccountUser/infoUser.js
+++ app/components/MyAccount/MyAccountUser/infoUser.js
@@ -4,6 +4,7 @@ import { Avatar } from 'react-native-elements';
 import * as firebase from 'firebase';
 import UpdateUserInfo from './UpdateUserInfo';
 import Toast, { DURATION } from 'react-native-easy-toast';
+import { ImagePicker, Permissions } from 'expo';
 
 export default class infoUser extends Component {
 	constructor(props) {
@@ -44,6 +45,14 @@ export default class infoUser extends Component {
 		this.getUserInfo();
 	};
 
+	updateUserPhotoURL = async (photoUri) => {
+		const update = { photoURL: photoUri };
+
+		await firebase.auth().currentUser.updateProfile(update);
+
+		this.getUserInfo();
+	};
+
 	returnUpdateUserInfoComponent = (userInfoData) => {
 		if (userInfoData.hasOwnProperty('uid')) {
 			return (
@@ -62,15 +71,71 @@ export default class infoUser extends Component {
 				user
 					.updateEmail(newEmail)
 					.then(() => {
-						this.refs.toast.show('Cambio de contraseña correcto', 1500);
-						firebase.auth().signOut();
+						this.refs.toast.show('Cambio de contraseña correcto, favor de iniciar sesión', 1500, () => {
+							firebase.auth().signOut();
+						});
 					})
 					.catch((err) => {
-						console.log(error);
+						this.refs.toast.show(error, 1500);
 					});
 			})
 			.catch((err) => {
 				this.refs.toast.show('Tu contraseña es incorrecta', 1500);
+			});
+	};
+	changeAvatarUser = async () => {
+		const resultPermission = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+
+		console.log(resultPermission);
+		if (resultPermission.status === 'denied') {
+			this.refs.toast.show('Es necesario aceptar los permisos para realixar esta accion', 1500);
+		} else {
+			const result = await ImagePicker.launchImageLibraryAsync({ allowsEditing: true, aspect: [ 4, 3 ] });
+			if (result.cancelled) {
+				this.refs.toast.show('Operacion cancelada', 1500);
+			} else {
+				const { uid } = this.state.userInfo;
+				this.upLoadImage(result.uri, uid)
+					.then((resolve) => {
+						this.refs.toast.show('Avatar actualizado correctamente', 1500);
+
+						firebase
+							.storage()
+							.ref('avatar/' + uid)
+							.getDownloadURL()
+							.then((resolve) => {
+								this.updateUserPhotoURL(resolve);
+								//console.log(resolve);
+							})
+							.catch((error) => {
+								this.refs.toast.show('Error al recuperar el avatar del servidor', 1500);
+							});
+					})
+					.catch((error) => {
+						this.refs.toast.show('Error al actualizar el avatar, intentar mas tarde', 1500);
+					});
+			}
+		}
+	};
+	upLoadImage = async (uri, nameImage) => {
+		return new Promise((resolve, reject) => {
+			let xhr = new XMLHttpRequest();
+			xhr.onerror = reject;
+			xhr.onreadystatechange = () => {
+				if (xhr.readyState === 4) {
+					resolve(xhr.response);
+				}
+			};
+			xhr.open('GET', uri);
+			xhr.responseType = 'blob';
+			xhr.send();
+		})
+			.then(async (resolve) => {
+				let ref = firebase.storage().ref().child('avatar/' + nameImage);
+				return await ref.put(resolve);
+			})
+			.catch((error) => {
+				this.refs.toast.show('Error al subir imagen intente mas tarde', 1500);
 			});
 	};
 	render() {
@@ -85,6 +150,8 @@ export default class infoUser extends Component {
 							uri: this.checkUserAvatar(photoUrl)
 						}}
 						containerStyle={styles.userInfoAvatar}
+						showEditButton
+						onEditPress={() => this.changeAvatarUser()}
 					/>
 					<View>
 						<Text style={styles.displayName}>{displayName}</Text>
