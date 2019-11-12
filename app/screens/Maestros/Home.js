66731bbd997c1369428b181d38bf3319d52386e9
UAHM <uahm93@gmail.com>
2019-11-11 18:56:40 -0600

    Scroll Infinito

--- app/screens/Maestros/Home.js
+++ app/screens/Maestros/Home.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { StyleSheet, View, Text, FlatList, ActivityIndicator } from 'react-native';
+import { StyleSheet, View, Text, FlatList, ActivityIndicator, TouchableOpacity } from 'react-native';
 import { white } from 'ansi-colors';
 import ActionButton from 'react-native-action-button';
 import Icon from 'react-native-vector-icons/Ionicons';
@@ -61,22 +61,66 @@ export default class Home extends Component {
 		});
 	};
 
+	handleLoadMore = async () => {
+		const { docente, limitDocente, startDocente } = this.state;
+		let resulDocentes = [];
+		const docentesDB = db
+			.collection('Docentes')
+			.orderBy('createat', 'desc')
+			.startAfter(startDocente.data().createat)
+			.limit(8);
+
+		// await docentesDB.get().then((response) => {
+		// 	if (response.docs.length > 0) {
+		// 		this.setState({
+		// 			startDocente: response.doc(response.docs.length - 1)
+		// 		});
+		// 	} else {
+		// 		this.setState({
+		// 			isLoading: false
+		// 		});
+		// 	}
+		// 	response.forEach((doc) => {
+		// 		let docente = doc.data();
+		// 		docente.id = doc.id;
+		// 		resulDocentes.push({ docente });
+		// 	});
+		// 	this.setState({
+		// 		docente: resulDocentes
+		// 	});
+		// });
+	};
+
+	renderFooter = () => {
+		if (this.state.isLoading) {
+			return <ActivityIndicator style={styles.loaderMas} size="large" />;
+		} else {
+			return (
+				<View style={styles.noFoundmore}>
+					<Text>Ya no hay mas registros</Text>
+				</View>
+			);
+		}
+	};
+
 	renderRow = (docentesObject) => {
 		const { name, school, image, facultad, description, city, createat } = docentesObject.item.docente;
 
 		return (
-			<View style={styles.viewDocentes}>
-				<View style={styles.viewDocenteImage}>
-					<Image resizeMode="cover" source={{ uri: image }} style={styles.imageDoente} />
+			<TouchableOpacity onPress={() => this.clickDocente(docentesObject)}>
+				<View style={styles.viewDocentes}>
+					<View style={styles.viewDocenteImage}>
+						<Image resizeMode="cover" source={{ uri: image }} style={styles.imageDoente} />
+					</View>
+					<View>
+						<Text style={styles.flatListName}> {name} </Text>
+						<Text style={styles.flatListAdress}>
+							{city}, {school}, {facultad}
+						</Text>
+						<Text style={styles.flatListDescription}>{description.substr(0, 60)}...</Text>
+					</View>
 				</View>
-				<View>
-					<Text style={styles.flatListName}> {name} </Text>
-					<Text style={styles.flatListAdress}>
-						{city}, {school}, {facultad}
-					</Text>
-					<Text style={styles.flatListDescription}>{description.substr(0, 60)}...</Text>
-				</View>
-			</View>
+			</TouchableOpacity>
 		);
 	};
 
@@ -87,6 +131,9 @@ export default class Home extends Component {
 					data={this.state.docente}
 					renderItem={this.renderRow}
 					keyExtractor={(item, index) => index.toString()}
+					onEndReached={this.handleLoadMore}
+					onEndReachedThreshold={0.1}
+					ListFooterComponent={this.renderFooter}
 				/>
 			);
 		} else {
@@ -97,6 +144,11 @@ export default class Home extends Component {
 				</View>
 			);
 		}
+	};
+
+	clickDocente = (docente) => {
+		console.log('haz dado click en ');
+		console.log(docente);
 	};
 
 	render() {
@@ -147,5 +199,14 @@ const styles = StyleSheet.create({
 		paddingTop: 2,
 		color: 'grey',
 		width: 300
+	},
+	loaderMas: {
+		marginTop: 10,
+		marginBottom: 10
+	},
+	noFoundmore: {
+		marginTop: 10,
+		marginBottom: 20,
+		alignItems: 'center'
 	}
 });
